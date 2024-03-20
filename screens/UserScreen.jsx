f2033704888ed62ae15ae7c705c97dd6c7db8642
Rohan Udhwani <rohanudhwani2@gmail.com>
2024-03-20 20:50:38 +0530

    Added avatar based on email

--- screens/UserScreen.jsx
+++ screens/UserScreen.jsx
@@ -1,5 +1,5 @@
-import { Button, Linking, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import { Button, Image, Linking, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { auth } from '../firebase';
 import { signOut } from 'firebase/auth'
@@ -10,6 +10,7 @@ const UserScreen = () => {
 
     const user = auth.currentUser;
     const navigation = useNavigation()
+    const email = user.email
 
     const handleSignOut = async () => {
         signOut(auth).then(() => {
@@ -29,8 +30,9 @@ const UserScreen = () => {
         <SafeAreaView style={{ justifyContent: 'center', alignItems: 'center', marginTop: 20, marginLeft: 20, marginRight: 20 }}>
             <StatusBar backgroundColor="#1D7C38" barStyle="dark-content" />
             <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 60, marginTop: 30 }}>User Details</Text>
-            <View>
-                <Text style={{ fontSize: 16, marginBottom: 30 }}>Email: {auth.currentUser.email}</Text>
+            <View style={{gap:15, justifyContent:"center", alignItems:"center"}}>
+                <Image source={{ uri: `https://ui-avatars.com/api/name=${email}?background=1D7C3&color=fff&size=128` }} style={{ width: 100, height: 100, borderRadius: 64 }} />
+                <Text style={{ fontSize: 16, marginBottom: 30 }}>Email: {email}</Text>
             </View>
             <TouchableOpacity onPress={() => handleSignOut()} style={{ marginTop: 30, marginLeft: 20, marginRight: 20, height: 50, backgroundColor: "#1D7C38", borderRadius: 10, justifyContent: "center", alignItems: "center", width: 200 }}>
                 <Text style={{ fontFamily: 'Inter_400Regular', fontSize: 15, color: "white", fontWeight: "bold" }}>Sign Out</Text>
